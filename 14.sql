476d333c0795396a5f9a129c371ba362cd743d89
nemiko007 <otoha.youjopero2@gmail.com>
2026-02-04 20:42:37 +0900

    14

---
+++ 14.sql
@@ -0,0 +1,11 @@
+UPDATE Movies
+SET Director = "John Lasseter"
+WHERE Title = "A Bug's Life";
+
+UPDATE Movies
+SET Year = 1999
+WHERE Title = "Toy Story 2";
+
+update Movies
+set Title = 'Toy Story 3', Director = 'Lee Unkrich'
+where Title = 'Toy Story 8';
