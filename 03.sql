39c9972927d3a7f161add644791a99624df3fa93
nemiko007 <otoha.youjopero2@gmail.com>
2026-02-04 20:23:55 +0900

    3

---
+++ 03.sql
@@ -0,0 +1,11 @@
+select * from Movies
+where Title like '%Toy Story%';
+
+select * from Movies
+where Director = 'John Lasseter';
+
+select * from Movies
+where Director != 'John Lasseter';
+
+select * from movies
+where Title like '%WALL-%';
